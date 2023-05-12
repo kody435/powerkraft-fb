eb94352513c22e6de1dc91ee1359d8487b43c8ba
Cody <bittusmaths@gmail.com>
2023-05-12 14:19:06 +0530

    Fixed and added many things

--- components/ProductList.jsx
+++ components/ProductList.jsx
@@ -9,6 +9,7 @@ import { database } from "../firebaseConfig";
 function ProductList() {
   const [eva, setEva] = useState("");
   const [alu, setAlu] = useState("");
+  const [blade, setBlade] = useState("");
 
   useEffect(() => {
     const colRef1 = collection(database, "products");
@@ -30,82 +31,146 @@ function ProductList() {
       });
       setAlu(products);
     });
+
+    const colRef3 = collection(database, "products");
+    const q3 = query(colRef3, where("cat", "==", "blade"), limit(4));
+    getDocs(q3).then((snapshot) => {
+      let products = [];
+      snapshot.docs.forEach((doc) => {
+        products.push({ ...doc.data(), id: doc.id });
+      });
+      setBlade(products);
+    });
   }, []);
+
+  const Blades = (
+    <div>
+      <div className="mx-2 flex flex-row items-center justify-between ">
+        <h2 className="text-xl lg:text-2xl font-medium ">Blades</h2>
+        <Link
+          href="/categories#alu"
+          className="text-md md:text-lg mx-0 md:mx-5 lg:mx-10 "
+        >
+          Shop Blades →
+        </Link>
+      </div>
+      <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-4 xl:grid-cols-4 m-2 gap-2 md:gap-3">
+        {blade &&
+          blade.map((doc, key) => (
+            <div
+              key={key}
+              href={`/products/${doc.slug}`}
+              className="shadow-lg ease-in-out delay-150 duration-300 hover:-translate-1 translate hover:scale-105 flex flex-col"
+            >
+              <br />
+              <Image
+                src={doc.img}
+                alt=""
+                className="w-full rounded p-3"
+                width={500}
+                height={500}
+              />
+
+              <div className="my-3 ml-4 justify-end items-end">
+                <h3 className="font-medium text-gray-900 group-hover:underline group-hover:underline-offset-4">
+                  {doc.title}
+                </h3>
+              </div>
+            </div>
+          ))}
+      </div>
+    </div>
+  );
+
+  const Alumiunium = (
+    <div>
+      <div className="mx-2 flex flex-row items-center justify-between ">
+        <h2 className="text-xl lg:text-2xl font-medium ">ALUMINIUM Case</h2>
+        <Link
+          href="/categories#alu"
+          className="text-md md:text-lg mx-0 md:mx-5 lg:mx-10 "
+        >
+          Shop Aluminium Case →
+        </Link>
+      </div>
+      <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-4 xl:grid-cols-4 m-2 gap-2 md:gap-3">
+        {alu &&
+          alu.map((doc, key) => (
+            <div
+              key={key}
+              href={`/products/${doc.slug}`}
+              className="shadow-lg ease-in-out delay-150 duration-300 hover:-translate-1 translate hover:scale-105 flex flex-col"
+            >
+              <br />
+              <Image
+                src={doc.img}
+                alt=""
+                className="w-full rounded p-3"
+                width={500}
+                height={500}
+              />
+
+              <div className="my-3 ml-4 justify-end items-end">
+                <h3 className="font-medium text-gray-900 group-hover:underline group-hover:underline-offset-4">
+                  {doc.title}
+                </h3>
+              </div>
+            </div>
+          ))}
+      </div>
+    </div>
+  );
+
+  const Evas = (
+    <div>
+      <div className="mx-2 flex flex-row items-center justify-between ">
+        <h2 className="text-xl lg:text-2xl font-medium ">EVA Case</h2>
+        <Link
+          href="/categories#eva"
+          className="text-md md:text-lg mx-0 md:mx-5 lg:mx-10 "
+        >
+          Shop Eva Case →
+        </Link>
+      </div>
+      <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-4 xl:grid-cols-4 m-2 gap-2 md:gap-3">
+        {eva &&
+          eva.map((doc, key) => (
+            <Link
+              key={key}
+              href={`/products/${doc.slug}`}
+              className="shadow-lg ease-in-out delay-150 duration-300 hover:-translate-1 translate hover:scale-105 flex flex-col"
+            >
+              <br />
+              <Image
+                src={doc.img}
+                alt=""
+                className="w-full rounded p-3"
+                width={500}
+                height={500}
+              />
+
+              <div className="my-3 ml-4 justify-end items-end">
+                <h3 className="font-medium text-gray-900 group-hover:underline group-hover:underline-offset-4 prose">
+                  {doc.title}
+                </h3>
+              </div>
+            </Link>
+          ))}
+      </div>
+    </div>
+  );
 
   return (
     <div className="gap-16 flex flex-col my-5 lg:my-10 xl:my-15">
+      
+      {/* Blades */}
+      {Blades}
+
       {/* EVA Case */}
-      <div>
-        <div className="mx-2 flex flex-row items-center justify-between ">
-          <h2 className="text-xl lg:text-2xl font-medium ">EVA Case</h2>
-          <p className="text-md md:text-lg mx-0 md:mx-5 lg:mx-10 ">
-            Shop Eva Case →
-          </p>
-        </div>
-        <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-4 xl:grid-cols-4 m-2 gap-2 md:gap-3">
-          {eva &&
-            eva.map((doc, key) => (
-              <div
-                key={key}
-                href={`/products/${doc.slug}`}
-                className="shadow-lg ease-in-out delay-150 duration-300 hover:-translate-1 translate hover:scale-105 flex flex-col"
-              >
-                <br />
-                <Image
-                  src={doc.img}
-                  alt=""
-                  className="w-full rounded p-3"
-                  width={500}
-                  height={500}
-                />
-
-                <div className="my-3 ml-4 justify-end items-end">
-                  <h3 className="font-medium text-gray-900 group-hover:underline group-hover:underline-offset-4 prose">
-                    {doc.title}
-                  </h3>
-                </div>
-              </div>
-            ))}
-        </div>
-      </div>
-
+      {Evas}
+      
       {/* Aluminium Case */}
-      <div>
-        <div className="mx-2 flex flex-row items-center justify-between ">
-          <h2 className="text-xl lg:text-2xl font-medium ">ALUMINIUM Case</h2>
-          <Link
-            href="/categories#alu"
-            className="text-md md:text-lg mx-0 md:mx-5 lg:mx-10 "
-          >
-            Shop Aluminium Case →
-          </Link>
-        </div>
-        <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-4 xl:grid-cols-4 m-2 gap-2 md:gap-3">
-          {alu &&
-            alu.map((doc, key) => (
-              <div
-                key={key}
-                href={`/products/${doc.slug}`}
-                className="shadow-lg ease-in-out delay-150 duration-300 hover:-translate-1 translate hover:scale-105 flex flex-col"
-              >
-                <br />
-                <Image
-                  src={doc.img}
-                  alt=""
-                  className="w-full rounded p-3"
-                  width={500}
-                  height={500}
-                />
-
-                <div className="my-3 ml-4 justify-end items-end">
-                  <h3 className="font-medium text-gray-900 group-hover:underline group-hover:underline-offset-4">
-                    {doc.title}
-                  </h3>
-                </div>
-              </div>
-            ))}
-        </div>
-      </div>
+      {Alumiunium}
     </div>
   );
 }
--- pages/categories.jsx
+++ pages/categories.jsx
@@ -10,6 +10,7 @@ import Head from "next/head";
 function ProductList() {
   const [eva, setEva] = useState("");
   const [alu, setAlu] = useState("");
+  const [blade, setBlade] = useState("");
 
   useEffect(() => {
     const colRef1 = collection(database, "products");
@@ -31,76 +32,126 @@ function ProductList() {
       });
       setAlu(products);
     });
+
+    const colRef3 = collection(database, "products");
+    const q3 = query(colRef3, where("cat", "==", "blade"));
+    getDocs(q3).then((snapshot) => {
+      let products = [];
+      snapshot.docs.forEach((doc) => {
+        products.push({ ...doc.data(), id: doc.id });
+      });
+      setBlade(products);
+    });
   }, []);
+
+  const Evas = (
+    <div>
+      <div className="mx-2 flex flex-row items-center justify-between ">
+        <h2 className="text-xl lg:text-2xl font-medium ">EVA Case</h2>
+      </div>
+      <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-4 xl:grid-cols-4 m-2 gap-2 md:gap-3">
+        {eva &&
+          eva.map((doc) => (
+            <div
+              key=""
+              href={`/products/${doc.slug}`}
+              className="shadow-lg ease-in-out delay-150 duration-300 hover:-translate-1 translate hover:scale-105 flex flex-col"
+            >
+              <br />
+              <Image
+                src={doc.img}
+                alt=""
+                className="w-full rounded p-3"
+                width={500}
+                height={500}
+              />
+
+              <div className="my-3 ml-4 justify-end items-end">
+                <h3 className="font-medium text-gray-900 group-hover:underline group-hover:underline-offset-4 prose">
+                  {doc.title}
+                </h3>
+              </div>
+            </div>
+          ))}
+      </div>
+    </div>
+  );
+
+  const Aluminium = (
+    <section id="alu">
+      <div className="mx-2 flex flex-row items-center justify-between ">
+        <h2 className="text-xl lg:text-2xl font-medium ">ALUMINIUM Case</h2>
+      </div>
+      <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-4 xl:grid-cols-4 m-2 gap-2 md:gap-3">
+        {alu &&
+          alu.map((doc) => (
+            <div
+              key=""
+              href={`/products/${doc.slug}`}
+              className="shadow-lg ease-in-out delay-150 duration-300 hover:-translate-1 translate hover:scale-105 flex flex-col"
+            >
+              <br />
+              <Image
+                src={doc.img}
+                alt=""
+                className="w-full rounded p-3"
+                width={500}
+                height={500}
+              />
+
+              <div className="my-3 ml-4 justify-end items-end">
+                <h3 className="font-medium text-gray-900 group-hover:underline group-hover:underline-offset-4">
+                  {doc.title}
+                </h3>
+              </div>
+            </div>
+          ))}
+      </div>
+    </section>
+  );
+
+  const Blades = (
+    <section id="alu">
+      <div className="mx-2 flex flex-row items-center justify-between ">
+        <h2 className="text-xl lg:text-2xl font-medium ">Blades</h2>
+      </div>
+      <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-4 xl:grid-cols-4 m-2 gap-2 md:gap-3">
+        {blade &&
+          blade.map((doc) => (
+            <div
+              key=""
+              href={`/products/${doc.slug}`}
+              className="shadow-lg ease-in-out delay-150 duration-300 hover:-translate-1 translate hover:scale-105 flex flex-col"
+            >
+              <br />
+              <Image
+                src={doc.img}
+                alt=""
+                className="w-full rounded p-3"
+                width={500}
+                height={500}
+              />
+
+              <div className="my-3 ml-4 justify-end items-end">
+                <h3 className="font-medium text-gray-900 group-hover:underline group-hover:underline-offset-4">
+                  {doc.title}
+                </h3>
+              </div>
+            </div>
+          ))}
+      </div>
+    </section>
+  );
 
   return (
     <div className="gap-16 flex flex-col my-5 lg:my-10 xl:my-15">
       <Head>
         <title>POWERKRAFT | Categories</title>
       </Head>
-      {/* EVA Case */}
-      <div>
-        <div className="mx-2 flex flex-row items-center justify-between ">
-          <h2 className="text-xl lg:text-2xl font-medium ">EVA Case</h2>
-        </div>
-        <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-4 xl:grid-cols-4 m-2 gap-2 md:gap-3">
-          {eva &&
-            eva.map((doc) => (
-              <div
-                key=""
-                href={`/products/${doc.slug}`}
-                className="shadow-lg ease-in-out delay-150 duration-300 hover:-translate-1 translate hover:scale-105 flex flex-col"
-              >
-                <br />
-                <Image
-                  src={doc.img}
-                  alt=""
-                  className="w-full rounded p-3"
-                  width={500}
-                  height={500}
-                />
+      {Blades}
+      {Evas}
+      {Aluminium}
 
-                <div className="my-3 ml-4 justify-end items-end">
-                  <h3 className="font-medium text-gray-900 group-hover:underline group-hover:underline-offset-4 prose">
-                    {doc.title}
-                  </h3>
-                </div>
-              </div>
-            ))}
-        </div>
-      </div>
-
-      {/* Aluminium Case */}
-      <section id="alu">
-        <div className="mx-2 flex flex-row items-center justify-between ">
-          <h2 className="text-xl lg:text-2xl font-medium ">ALUMINIUM Case</h2>
-        </div>
-        <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-4 xl:grid-cols-4 m-2 gap-2 md:gap-3">
-          {alu &&
-            alu.map((doc) => (
-              <div
-                key=""
-                href={`/products/${doc.slug}`}
-                className="shadow-lg ease-in-out delay-150 duration-300 hover:-translate-1 translate hover:scale-105 flex flex-col"
-              >
-                <br />
-                <Image
-                  src={doc.img}
-                  alt=""
-                  className="w-full rounded p-3"
-                  width={500}
-                  height={500}
-                />
-
-                <div className="my-3 ml-4 justify-end items-end">
-                  <h3 className="font-medium text-gray-900 group-hover:underline group-hover:underline-offset-4">
-                    {doc.title}
-                  </h3>
-                </div>
-              </div>
-            ))}
-        </div>
-      </section>
     </div>
   );
 }
